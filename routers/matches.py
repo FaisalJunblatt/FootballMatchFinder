ae485fd49e1845e9db1625daec5760624276932f
FaisalJunblatt <fjunblatt.ieu2023@student.ie.edu>
2025-09-26 21:17:58 +0200

    feat: added a match

--- routers/matches.py
+++ routers/matches.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, select
 from db import get_session
 from models import Match, MatchCreate, MatchRead
 
-# correct: lowercase "prefix"
+
 router = APIRouter(prefix="/matches", tags=["matches"])
 
 @router.post("", response_model=MatchRead, status_code=201)
@@ -17,3 +17,16 @@ def create_match(payload: MatchCreate, session: Session = Depends(get_session)):
 @router.get("", response_model=list[MatchRead])
 def list_matches(session: Session = Depends(get_session)):
     return session.exec(select(Match).order_by(Match.date, Match.time)).all()
+
+@router.put("/{match_id}/join", response_model=MatchRead)   
+def join_math(match_id: int, session: Session = Depends(get_session)):
+    match=session.get(Match,match_id)
+    if not match:
+        raise HTTPException(status_code=404, detail="Match not found")
+    if match.joined_players >= match.max_players:
+        raise HTTPException(status_code=400, detail="Match is full")
+    match.joined_players += 1
+    session.add(match)
+    session.commit()
+    session.refresh(match)
+    return match
